a917a4e0a94bd802f48cbbd021a97341e943073e
YassineZhang <57878778@qq.com>
2024-12-13 19:57:55 +0800

    refactor: 优化markdown chunks以及扩展高亮代码块

--- src/composables/useChunkedMarkdownParser.ts
+++ src/composables/useChunkedMarkdownParser.ts
@@ -1,16 +1,28 @@
 import { parseMarkdown } from "@/utils/markdownUtils";
+import { marked } from "marked";
 
 export function useChunkedMarkdownParser() {
   const parseInChunks = async (
     markdownContent: string,
     updateProgress: (progress: number) => void,
   ) => {
-    const chunks = markdownContent.split("\n\n"); // 简单的分块方式
+    // 使用 Marked 的 Lexer 解析
+    const tokens = marked.lexer(markdownContent);
+
+    // 转换 Token 为字符串
+    const chunks = tokens.map((token) => {
+      if (token.type === "paragraph") return token.raw;
+      if (token.type === "heading") return `${token.raw}`;
+      if (token.type === "code")
+        return `\`\`\`${token.lang || ""}\n${token.text}\n\`\`\``;
+      return token.raw || "";
+    });
     const totalChunks = chunks.length;
     let parsedContent = "";
 
     for (let i = 0; i < totalChunks; i++) {
       parsedContent += await parseMarkdown(chunks[i]);
+
       const progress = ((i + 1) / totalChunks) * 100;
       updateProgress(progress); // 使用传入的函数更新进度
     }
--- src/utils/markdownUtils.ts
+++ src/utils/markdownUtils.ts
@@ -1,18 +1,20 @@
 import { marked } from "marked";
 import { createStarryNight, common } from "@wooorm/starry-night";
+import sourceNginx from "@wooorm/starry-night/source.nginx";
 import { toHtml } from "hast-util-to-html";
 import DOMPurify from "dompurify";
 
-export async function parseMarkdown(markdown: string): Promise<string> {
-  const starryNight = await createStarryNight(common);
+const starryNight = await createStarryNight([...common, sourceNginx]);
 
+export async function parseMarkdown(markdown: string): Promise<string> {
   // Renderer
   const renderer = new marked.Renderer();
   renderer.code = ({ text, lang }) => {
     const scope =
       starryNight.flagToScope(`source.${lang}`) ||
-      starryNight.flagToScope("text.plain") ||
-      "text.plain";
+      starryNight.flagToScope("text.md") ||
+      "text.md";
+
     const tree = starryNight.highlight(text, scope);
     return `<pre><code>${toHtml(tree)}</code></pre>`;
   };
